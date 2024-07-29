7e793050c443832eb95fee35968616c6fe7ee190
mfakiri <mouhcinedev@gmail.com>
2024-07-29 17:24:46 +0100

    add relations

--- handlers/artist.go
+++ handlers/artist.go
@@ -13,49 +13,75 @@ import (
 type locations struct {
 	LocationsD []string `json:"locations"`
 }
-type Data struct {
-	Old Res
-	Loc locations
+type dates struct {
+	DatesD []string `json:"dates"`
+}
+type relation struct {
+	RelationD map[string][]string `json:"datesLocations"`
 }
 
+type Data struct {
+	Old      Res
+	Loc      locations
+	Date     dates
+	Relation relation
+}
+
+var (
+	newlocation locations
+	Dates       dates
+	Relation    relation
+)
+
 func GetArtist(res http.ResponseWriter, req *http.Request) {
-	var newlocation locations
 	file := "artist.html"
 
 	tmpl, err := template.ParseFiles(file)
 	if err != nil {
 		fmt.Println(err)
 	}
-	// id, _ := fmt.Println("id : ", id)
+
 	elem := strings.Split(string(req.URL.Path), "/")
 	id, _ := strconv.Atoi(elem[len(elem)-1])
-	LocationURL := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/locations/%v", id)
 
-	locationsData, err1 := http.Get(LocationURL)
-
-	if err1 != nil {
-		fmt.Println(err1)
-	}
-
-	content, err2 := io.ReadAll(locationsData.Body)
-
-	json.Unmarshal(content, &newlocation)
-	fmt.Println("\n\ncontent: ", newlocation)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
-
+	getDates(id, "https://groupietrackers.herokuapp.com/api/locations/", "locations")
+	getDates(id, "https://groupietrackers.herokuapp.com/api/dates/", "dates")
+	getDates(id, "https://groupietrackers.herokuapp.com/api/relation/", "relation")
 	if id > 51 || id < 0 {
 		http.NotFound(res, req)
 		return
 	}
 	result := Data{
-		Old: ArtistsObj[id-1],
-		Loc: newlocation,
+		Old:      ArtistsObj[id-1],
+		Loc:      newlocation,
+		Date:     Dates,
+		Relation: Relation,
 	}
 
 	err = tmpl.Execute(res, result)
 	if err != nil {
 		fmt.Println(err)
+	}
+}
+
+func getDates(id int, url string, info string) {
+	DatesURL := fmt.Sprint(url, id)
+
+	DatesData, err := http.Get(DatesURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	content, err1 := io.ReadAll(DatesData.Body)
+
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	if info == "locations" {
+		json.Unmarshal(content, &newlocation)
+	} else if info == "dates" {
+		json.Unmarshal(content, &Dates)
+	} else if info == "relation" {
+		json.Unmarshal(content, &Relation)
 	}
 }
