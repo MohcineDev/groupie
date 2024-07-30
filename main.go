4b772474498bdc8fa872f83ce71781bfcec1c5cf
mfakiri <mouhcinedev@gmail.com>
2024-07-30 12:01:26 +0100

    handle errors

--- main.go
+++ main.go
@@ -8,16 +8,15 @@ import (
 )
 
 func main() {
+	///server paths
+	http.HandleFunc("/", handlers.HandleHome)
+	http.HandleFunc("/artists", handlers.GetArtist)
+
 	////serve style
 	http.Handle("/styles/", http.StripPrefix("/styles", http.FileServer(http.Dir("./styles"))))
 
-	///server
-	http.HandleFunc("/", handlers.HandleHome)
-	http.HandleFunc("/artists/", handlers.GetArtist)
-	handlers.GetArtistsData()
+	// run server
 	const PORT = "8000"
 	fmt.Printf("running on %v...", PORT)
 	http.ListenAndServe("localhost:"+PORT, nil)
-
-	///Template
 }
