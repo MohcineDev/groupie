998f9bfebcf894670e135755e5d09964089ef631
MohcineDev <mouhcinedev@gmail.com>
2024-08-08 00:22:55 +0100

    add env port

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 )
 
 func main() {
-	///server paths
-	http.HandleFunc("/", handlers.HandleHome)
-	http.HandleFunc("/artists", handlers.GetArtist)
-
 	////serve style
 	http.Handle("/styles/", http.StripPrefix("/styles", http.FileServer(http.Dir("./styles"))))
+
+	///server
+	http.HandleFunc("/", handlers.HandleHome)
+	http.HandleFunc("/artists/", handlers.GetArtist)
 
 	PORT := os.Getenv("PORT")
 
@@ -24,7 +24,7 @@ func main() {
 	}
 
 	fmt.Printf("running on %v...", PORT)
-	http.ListenAndServe("localhost:"+PORT, nil)
+	http.ListenAndServe("0.0.0.0:"+PORT, nil)
 
 	///Template
 }
